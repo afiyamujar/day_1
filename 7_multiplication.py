87c60d4ffa742bb67e13c79e474c1bed2b14fdf5
afiyamujar <afiya.mujar@gmail.com>
2025-06-19 14:31:16 +0530

    Update 7_multiplication.py

--- 7_multiplication.py
+++ 7_multiplication.py
@@ -1,6 +1,6 @@
-num=int (input("Enter a number ;"))
-multiplier =0 
-while multiplier <=10:
-   result = num*multiplier
-   multiplier+=1
-   print (result)
+num = int(input("Enter a number :"))
+multiplier = 1
+while multiplier <= 10:
+   result = num * multiplier
+   print(num, "X" , multiplier, "=", result)
+   multiplier +=1
