08aa267514885b7b5e273d325fd5e344a357c455
afiyamujar <afiya.mujar@gmail.com>
2025-06-19 14:21:19 +0530

    Update 4_userprint.py

--- 4_userprint.py
+++ 4_userprint.py
@@ -1,7 +1,7 @@
-num=int(input ("Enter a number :"))
-if num>0:
- print (num,"is a positive number ")
- elif num<0:
-  print (num,"is a negitive number ")
-  else:
-  print(num,"is a zero ")
+num = int(input ("Enter a number:"))
+if num > 0:
+     print(num,"is a positive number")
+elif num < 0:
+     print(num,"is a negative number")
+else:
+     print(num,"is  zero")
